f34907cd1d2ebb3a5a8f25b4430a3d22a57e1114
Prishnee Seegoolam <Prseegoolam@umail.utm.ac.mu>
2024-05-03 14:19:54 +0400

    about

---
+++ client/about/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3JL6AP5C.mjs";import"./chunk-O3IVYJGS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- client/dist/dashboard/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-FJIJSSTC.mjs";import"./chunk-C42VWWWR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
